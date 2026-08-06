4a5b84c426a79c63796e03cd3180dd1ade5587da
Adam Czapla <adam@adamczapla.de>
2026-08-06 07:08:08 +0200

    Add Final type annotations to path constants

--- src/paths.py
+++ src/paths.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+from typing import Final
 
-PROJECT_ROOT = Path(__file__).parent.parent
-DATA_DIR = PROJECT_ROOT / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
-IMPORTED_DATA_DIR = DATA_DIR / "imported"
-CLEANED_DATA_DIR = DATA_DIR / "cleaned"
-TRANSFORMED_DATA_DIR = DATA_DIR / "transformed"
+PROJECT_ROOT: Final = Path(__file__).parent.parent
+DATA_DIR: Final = PROJECT_ROOT / "data"
+RAW_DATA_DIR: Final = DATA_DIR / "raw"
+IMPORTED_DATA_DIR: Final = DATA_DIR / "imported"
+CLEANED_DATA_DIR: Final = DATA_DIR / "cleaned"
+TRANSFORMED_DATA_DIR: Final = DATA_DIR / "transformed"
